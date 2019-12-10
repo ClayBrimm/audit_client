bda82041fa24e0b1fe61d2e18510c8e723996c0d
Clayton Brimm <clayton@Carlton-MacBook.local>
2019-12-10 15:59:03 -0500

    Added change indicator logic

--- small.js
+++ small.js
@@ -11,7 +11,11 @@ function scrapeForm() {
     var end = new Date(range_end).getTime();
     var end_date = Math.floor(end / 1000);
     
-    var rehire_checkbox = document.getElementById("rehire").checked
+    //var rehire_checkbox = document.getElementById("rehire").checked
+    var add_checkbox = document.getElementById("add").checked
+    var delete_checkbox = document.getElementById("delete").checked
+    var reactivate_checkbox = document.getElementById("reactivate").checked
+    var change_checkbox = document.getElementById("change").checked
     
     var wildObj = new Object();
     var sendObj = new Object();
@@ -19,21 +23,37 @@ function scrapeForm() {
     sendObj.employee_identity = employee_id;
     sendObj.employer_name = employer_name;
     
-    //check if start_date and end_date are defined and then add them to an object
-    //and turn that object into a json string
-    if(typeof start_date !== 'undefined' && typeof end_date !== 'undefined' || rehire_checkbox){
-        if(typeof start_date !== 'undefined' && typeof end_date !== 'undefined'){
-            wildObj.start_range =  start_date.toString();
-            wildObj.end_range = end_date.toString();
-        }
-        if(rehire_checkbox){
-            wildObj.rehire = true;
-        }
-        var jsonString = JSON.stringify(wildObj);
-        console.log(jsonString);
-        sendObj.wildcards = jsonString;
+    //check if start_date and end_date are defined and then add them to an object and turn that object into a json string
+    
+    //TODO: This will create a wildcards json string that uses key value pairs for each possible search parameter. This could be changed to a list of fields. The backend would need to be adjusted
+    
+    if(start_date && end_date){
+        wildObj.start_range =  start_date.toString();
+        wildObj.end_range = end_date.toString();
     }
-
+    //if(rehire_checkbox){
+    //    wildObj.rehire = true;
+    //}
+    if(add_checkbox){
+        wildObj.add = true;
+    }
+    if(delete_checkbox){
+        wildObj.delete = true;
+    }
+    if(reactivate_checkbox){
+        wildObj.reactivate = true;
+    }
+    if(change_checkbox){
+        wildObj.change = true;
+    }
+    
+    /* EXAMPLE OF SENT JSON STRING 12.10.19
+   {"start_range":"1388966400","end_range":"1575936000","rehire":true,"add":true,"delete":true,"reactivate":true,"change":true}
+    */
+    
+    var jsonString = JSON.stringify(wildObj);
+    console.log(jsonString);
+    sendObj.wildcards = jsonString;
     
     var scrapedData = JSON.stringify(sendObj);
         
@@ -59,8 +79,19 @@ function scrapeForm() {
     $.ajax(settings).done(function (response) {
         console.log(response);
         alert(JSON.stringify(response));
+        var result = JSON.parse(response)
+        writeJSON(result);
         document.getElementById("json").innerHTML = response;
     });
     
 }//End of scrapeForm()
+
+function writeJSON(data){
+    for(var key in data){
+        if(data.hasOwnProperty(key)){
+            var value = data[key];
+            console.log(value['first_name']);
+        }
+    }
+}
 
